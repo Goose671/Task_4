7049719a75f8110150b312e20e80f9d87fd68d13
Vitalii Ilin <vitalii_ilin@epam.com>
2022-07-24 23:26:01 +0300

    rrefactoring

--- main.js
+++ main.js
@@ -1,53 +1,44 @@
 let slideNumber = 0;
-let numberSlide = 1
-let album = []
-let albumLength
-  axios.get('https://rickandmortyapi.com/api/character')
+let album = [];
+let albumLength;
+
+axios.get('https://rickandmortyapi.com/api/character')
     .then(function (response) {
-        album = response.data.results
-        albumLength = album.length
-        renderSlide(album)
+        album = response.data.results;
+        albumLength = album.length;
+        renderSlide(album);
     });
 
 const slide = document.querySelector('.img');
 const leftBtn = document.querySelector('.left');
 const rightBtn = document.querySelector('.right');
-const number = document.querySelector('.number');
+const slideCounter = document.querySelector('#slide-counter');
 leftBtn.addEventListener('click', prev);
 rightBtn.addEventListener('click', next);
 
 function renderSlide(response) {
-    slide.src = response[slideNumber].image
-    number.innerHTML = numberSlide + ' / ' + albumLength
-    
+    slide.src = response[slideNumber].image;
+    slideCounter.innerHTML = slideNumber + 1 + ' / ' + albumLength;
 };
 
 
 function prev() {
-    if ((slideNumber > 0) && (numberSlide > 0)) {
+    if (slideNumber > 0) {
         slideNumber--;
-        numberSlide--;
-        console.log(slideNumber)
-        renderSlide(album)
+        renderSlide(album);
     } else {
-        slideNumber = albumLength - 1
-        numberSlide = albumLength
-        console.log(slideNumber)
-        renderSlide(album)
+        slideNumber = albumLength - 1;
+        renderSlide(album);
     }
 };
 
 function next() {
-    if ((slideNumber < albumLength) && (numberSlide < albumLength)) {
+    if (slideNumber < albumLength - 1) {
         slideNumber++;
-        numberSlide++;
-        console.log(slideNumber)
-        renderSlide(album)
+        renderSlide(album);
     } else {
-        slideNumber = 0
-        numberSlide = 1
-        console.log(slideNumber)
-        renderSlide(album)
+        slideNumber = 0;
+        renderSlide(album);
     }
 };
 
